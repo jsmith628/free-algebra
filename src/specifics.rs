349ac232bf9c9269a2d32041b5bb8db20505d8c0
Joshua Smith <anvil775@gmail.com>
2019-11-06 01:23:38 -0600

    added some doc tests

--- src/specifics.rs
+++ src/specifics.rs
@@ -17,14 +17,14 @@ impl<C> MonoidRule<C> for () {
 ///Used for constructing [FreeGroup]
 ///
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
-pub enum FreeInv<T> {
+pub enum FreeInv<T:Eq> {
     ///Wraps an instance of type `T`
     Id(T),
     ///The symbolic inverse of an object of type `T`
     Inv(T)
 }
 
-impl<T:Display> Display for FreeInv<T> {
+impl<T:Eq+Display> Display for FreeInv<T> {
     fn fmt(&self, f: &mut Formatter) -> ::std::fmt::Result {
         match self {
             Self::Id(x) => write!(f, "{}", x),
@@ -33,21 +33,37 @@ impl<T:Display> Display for FreeInv<T> {
     }
 }
 
-impl<T> FreeInv<T> {
+impl<T:Eq> FreeInv<T> {
+
     pub fn is_inv(&self) -> bool {
         match self { Self::Inv(_) => true, _ => false }
     }
+
     pub fn is_id(&self) -> bool {
         match self { Self::Id(_) => true, _ => false }
+    }
+
+    pub fn are_inverses(&self, rhs:&Self) -> bool {
+        use FreeInv::*;
+        match self {
+            Id(x) => match rhs {
+                Id(_) => false,
+                Inv(y) => x==y
+            },
+            Inv(x) => match rhs {
+                Id(y) => x==y,
+                Inv(_) => false
+            }
+        }
     }
 }
 
 ///Multiplication of [FreeInv] elements using concatenation with inverse cancellation
 pub struct InvRule;
 
-impl<T> MonoidRule<FreeInv<T>> for InvRule {
+impl<T:Eq> MonoidRule<FreeInv<T>> for InvRule {
     fn apply(mut string: Vec<FreeInv<T>>, letter: FreeInv<T>) -> Vec<FreeInv<T>> {
-        if string.last().map_or(false, |last| letter.is_inv() ^ last.is_inv()) {
+        if string.last().map_or(false, |last| letter.are_inverses(last)) {
             string.pop();
         } else {
             string.push(letter);
@@ -56,11 +72,11 @@ impl<T> MonoidRule<FreeInv<T>> for InvRule {
     }
 }
 
-impl<T> InvMonoidRule<FreeInv<T>> for InvRule {
+impl<T:Eq> InvMonoidRule<FreeInv<T>> for InvRule {
     fn invert(letter: FreeInv<T>) -> FreeInv<T> { letter.inv() }
 }
 
-impl<T> Inv for FreeInv<T> {
+impl<T:Eq> Inv for FreeInv<T> {
     type Output = Self;
     fn inv(self) -> Self {
         match self {
@@ -70,22 +86,22 @@ impl<T> Inv for FreeInv<T> {
     }
 }
 
-impl<T> Mul for FreeInv<T> {
+impl<T:Eq> Mul for FreeInv<T> {
     type Output = FreeGroup<T>;
     fn mul(self, rhs:Self) -> FreeGroup<T> { FreeGroup::from(self) * rhs }
 }
 
-impl<T> Div for FreeInv<T> {
+impl<T:Eq> Div for FreeInv<T> {
     type Output = FreeGroup<T>;
     fn div(self, rhs:Self) -> FreeGroup<T> { self * rhs.inv() }
 }
 
-impl<T> Mul<FreeGroup<T>> for FreeInv<T> {
+impl<T:Eq> Mul<FreeGroup<T>> for FreeInv<T> {
     type Output = FreeGroup<T>;
     fn mul(self, rhs:FreeGroup<T>) -> FreeGroup<T> { FreeGroup::from(self) * rhs }
 }
 
-impl<T> Div<FreeGroup<T>> for FreeInv<T> {
+impl<T:Eq> Div<FreeGroup<T>> for FreeInv<T> {
     type Output = FreeGroup<T>;
     fn div(self, rhs:FreeGroup<T>) -> FreeGroup<T> { FreeGroup::from(self) / rhs }
 }
@@ -97,9 +113,49 @@ impl<T> Div<FreeGroup<T>> for FreeInv<T> {
 ///of members of `C` where multiplication is given by concatenation. In other words, it's basically
 ///[`Vec<C>`](Vec) but with `a*b == {a.append(&mut b); a}`.
 ///
+///```
+///# use maths_traits::algebra::*;
+///# use free_algebra::FreeMonoid;
+///#
+///let s1 = FreeMonoid::one() * 'a' * 'b' * 'c';
+///let s2 = s1.clone().reverse();
+///
+///assert_eq!(s1, ['a','b','c']);
+///assert_eq!(s2, ['c','b','a']);
+///
+///let mul = s1 * s2;
+///
+///assert_eq!(mul, ['a','b','c','c','b','a']);
+///
+///```
+///
 pub type FreeMonoid<C> = MonoidalString<C,()>;
 
+///
 ///A [FreeMonoid], but where each letter can be inverted
+///
+///In essence, this entails constructing a [FreeMonoid] over [FreeInv<C>](FreeInv) with the same
+///multiplication _except_ that elements cancel with their inverses
+///
+///```
+///# use maths_traits::algebra::*;
+///# use free_algebra::FreeGroup;
+///#
+///use free_algebra::FreeInv::*;
+///
+///let g1 = Id('a') * Id('b').inv() * Id('c');
+///let g2 = (&g1).inv();
+///
+///assert_eq!(g1, [Id('a'), Inv('b'), Id('c')]);
+///assert_eq!(g2, [Inv('c'), Id('b'), Inv('a')]);
+///assert!((&g1 * &g2).is_one());
+///assert!((&g2 * &g1).is_one());
+///
+///let mul = g1 * Id('d') * g2;
+///assert_eq!(mul, [Id('a'), Inv('b'), Id('c'), Id('d'), Inv('c'), Id('b'), Inv('a')]);
+///
+///```
+///
 pub type FreeGroup<C> = MonoidalString<FreeInv<C>,InvRule>;
 
 ///
