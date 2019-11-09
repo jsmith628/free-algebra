8abb794d1a43cb0b9d3cf9dcfbf849af4fab4a96
Joshua Smith <anvil775@gmail.com>
2019-11-08 23:39:37 -0600

    edited crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,20 @@
 //!
 //!# What even is a "Free Algebra"?
 //!
-//!In the context of this crate, the term "Algebra" refers to the general branch of mathematical
+//!In the context of this crate, the term "Algebra" refers to a range of mathematical
 //!constructions involving arithmetic operations, and the term "Free" refers to the nature of those
 //!operations. In particular, a "free" operation is one that is made with as little restriction
-//!as possible without violating the algebraic rules that are desired.
+//!as possible with respect to the desired set of rules.
 //!
-//!Hence, in general, the procedure to make such a "free" construction is to start with some type `T`
-//!(and it could really be any type), and some set of algebraic rules, and then simply apply them
-//!to elements of `T` as if they were a variable or symbol.
+//!So, in general, the procedure for such a "free" construction is to start with some type `T` and
+//!some set of algebraic rules, and then operated on the elements of `T` as if they were a variable
+//!or symbol, applying the rules as necessary.
 //!
 //!As abstract as that sounds, there is actually a prime example of this already in the standard
 //!library, the [`Vec<T>`](Vec)! If we start with some type `T`, assert that multiplication be associative,
-//!and start multiplying elements as variables, the result is exactly the same as if we took
-//![`Vec<T>`](Vec) and implemented concatenation as [multiplication](Mul) and the empty list as
-//![`1`](One) In fact, the type [`FreeMonoid<T>`](FreeMonoid) in this crate is *precisely* that.
+//!and start multiplying elements like variables, the result is exactly the same as if we took
+//![`Vec<T>`](Vec) and implemented [multiplication](Mul) as concatenation. In fact, this is *precisely*
+//!what the  [`FreeMonoid<T>`](FreeMonoid) type in this crate is.
 //!
 //!```
 //!use maths_traits::algebra::One;
@@ -34,17 +34,17 @@
 //!
 //!```
 //!
-//!In addition to this, however, there are a number of other constructions you can get by changing
-//!what types are used, which operations are considered, and what rules are followed. Examples include:
-//! * [`FreeModule<R,T>`](FreeModule): results from requiring that elements of `T` can be added
-//!   together commutatively and associatively and can be multiplied distributively by the scalar
-//!   type `R`
-//! * [`FreeAlgebra<R,T>`](FreeAlgebra): This is the same as [FreeModule], except that we let `T`
-//!   multiply distributively with itself freely similarly as with [FreeMonoid]
+//!In addition to this, moreover, a number of other constructions can be acheived by changing
+//!which types are used, which operations are considered, and what rules are followed.
+//!Examples include:
+//! * [`FreeModule<R,T>`](FreeModule): Results from freely adding elements of `T` in an associative
+//!  and commutative manner and allowing distributive multiplication by elements from `R`
+//! * [`FreeAlgebra<R,T>`](FreeAlgebra): The same as with [FreeModule], except that we allow for
+//!  free multiplication of elements distributively (like with [FreeMonoid])
 //! * Polynomials: A [FreeAlgebra], but where multiplication between `T`'s is commutative and associative
 //! * Clifford algebra: A [FreeAlgebra], but where multiplication is associative and
 //!   an element times itself results in a scalars
-//! * Complex numbers: Results from when `T` includes `1` and `i` and multiplies accordingly
+//! * Complex numbers: Results from when `T` is either `1` and `i` and multiplies accordingly
 //! * Quaternions: Same as for Complex numbers, but with more imaginary units
 //!
 //!# Crate structures
@@ -56,12 +56,13 @@
 //!
 //!Specifically:
 //! * [MonoidalString] constructs free-multiplying structures over a type `T` using an order-dependent
-//!   internal representation with a [`Vec<T>`](Vec) and determines its multiplication rule using an
-//!   implementor of [MonoidRule]. Aliases of this struct include [FreeMonoid] and [FreeGroup]
+//!   internal representation with a [`Vec<T>`](Vec) that determines its multiplication rule using an
+//!   implementor of the trait [MonoidRule]. Aliases of this struct include [FreeMonoid] and [FreeGroup].
 //! * [ModuleString] constructs types consisting of terms of type `T` with scalars from
 //!   some additive type `R` stored with an order independent [HashMap]. This grants all [ModuleString]'s
-//!   an addition operation from adding the coeffients of like terms, but a free-multiplication can
-//!   be added with an optional [AlgebraRule]. Aliases of this struct include [FreeModule] and [FreeAlgebra]
+//!   an addition operation by adding the coeffients of like terms, and a free-multiplication can
+//!   be included using an optional [AlgebraRule] parameter. Aliases of this struct include
+//!   [FreeModule] and [FreeAlgebra].
 //!
 //!For more information, see the respective structs
 //!
